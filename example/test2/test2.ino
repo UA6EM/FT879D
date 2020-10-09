d319182b87822d11cc95863d70889ff6a9d38f75
Victor <ua6em@yandex.ru>
2020-10-09 13:29:22 +0000

    Update test2.ino

--- example/test2/test2.ino
+++ example/test2/test2.ino
@@ -1,27 +1,28 @@
 /*
- Name:		Test_FT897D.ino
- Created:	25.09.2020 7:06:13
- Author:	DtS
+ Name:    Test2.ino
+ Created: 09.10.2020 
+ Author:  UA6EM
 */
 float ftFreq;
 #include <SoftwareSerial.h>
 #include "dtsFT897D.h"
 
-SoftwareSerial RadioPort(8,7);
+SoftwareSerial RadioPort(2,3);
 
 dtsFT897D Radio(RadioPort);
 
 void setup() {
-	Serial.begin(115200);
-	delay(250);
-	Serial.println("Start...");
-	Radio.Init(4800);
+  Serial.begin(115200);
+  delay(250);
+  Serial.println("Start...");
+  Radio.Init(4800);
 }
 
 
 void loop() {
-	
-	ftFreq = Radio.GetFrequency();
-	Serial.println("Freq = ",ftFreq);
-	delay(5000);
+  
+  ftFreq = Radio.GetFrequency();
+  Serial.print("Freq = ");
+  Serial.println(ftFreq);
+  delay(5000);
 }
