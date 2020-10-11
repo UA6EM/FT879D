0a486984fb21429eb9c44f939af646057a04b266
Victor <ua6em@yandex.ru>
2020-10-11 09:33:47 +0300

    Create getFreq.ino
    Получение рабочей частоты от трансивера

---
+++ example/getFreq/getFreq.ino
@@ -0,0 +1,28 @@
+/*
+ Name:    Test_FT897D.ino
+ Created: 25.09.2020 7:06:13
+ Author:  DtS
+*/
+float ftFreq;
+#include <SoftwareSerial.h>
+#include "dtsFT897D.h"
+
+SoftwareSerial RadioPort(2,3); // RX, TX
+
+dtsFT897D Radio(RadioPort);
+
+void setup() {
+  Serial.begin(115200);
+  delay(250);
+  Serial.println("Start...");
+  Radio.Init(4800);
+}
+
+
+void loop() {
+  
+  ftFreq = Radio.GetFrequency();
+  Serial.print("Freq = ");
+  Serial.println(ftFreq);
+  delay(2000);
+}
